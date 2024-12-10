8103c38f8a077c617d92faf3d6b1cd7110a34f3f
Erik Pohl <epohl@dmigs.com>
2024-12-10 09:15:50 -0500

    added in intersection class

--- main.py
+++ main.py
@@ -4,6 +4,57 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
+class Directions:
+    rose = [
+        "north",
+        "east",
+        "southeast",
+        "northeast",
+        "up",
+        "down",
+        "northwest",
+        "southwest",
+        "west",
+        "south"
+    ]
+
+    def opposite(self, direction):
+        return self.rose[len(self.rose)-self.rose.index(direction)-1]
+
+
+class Intersection:
+    dirs = Directions()
+
+    def __init__(self, name):
+        self.name = name
+        self.exits = {
+            "north": None,
+            "south": None,
+            "east": None,
+            "west": None,
+            "southeast": None,
+            "southwest": None,
+            "northwest": None,
+            "northeast": None,
+            "up": None,
+            "down": None
+        }
+        self.is_finish = False
+        self.is_start = False
+
+    def output(self):
+        print(self.name)
+        for direction in self.dirs.rose:
+            if self.exits[direction]:
+                print(f"Exit to {direction} {self.exits[direction].name}")
+
+    def make_exit(self, direction, location):
+        if direction not in self.dirs.rose:
+            raise ValueError
+        self.exits[direction] = location
+        opp_dir = self.dirs.opposite(direction)
+        location.exits[opp_dir] = self
+
 class Poll:
     name = "Erik"
     question = "why??????"
@@ -20,5 +71,10 @@ def index():
 
 
 if __name__ == "__main__":
-    poll = Poll()
-    app.run()
+    xection = Intersection("One")
+    place = Intersection("Two")
+    xection.make_exit("north", place)
+    xection.output()
+    place.output()
+    # poll = Poll()
+    # app.run()
