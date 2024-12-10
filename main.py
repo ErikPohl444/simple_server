415120772e0dd2d326730d1a8fd621b930669409
Erik Pohl <epohl@dmigs.com>
2024-12-09 22:18:58 -0500

    added index html and also gitignore and referenced a class in index html

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+class Poll:
+    name = "Erik"
+    question = "why??????"
+
+@app.route('/goodbye/<name>')
+def hello(name):
+    return f"<h1>Goodbye, !!!!!{escape(name)}!</h1>"
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html', hostname="hostname", poll=poll)
+
+
+if __name__ == "__main__":
+    poll = Poll()
+    app.run()
