40fdefe767a996aa02099835fe9bb401e50957c7
Erik Pohl <epohl@dmigs.com>
2025-02-07 22:31:04 -0500

    upgraded logging

--- setup_logging.py
+++ setup_logging.py
@@ -2,10 +2,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+handler = logging.FileHandler('my_app.log')
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler()
+handler2 = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+logger.addHandler(handler2)
+
 
 def call_log(func):
     def wrapper(*args, **kwargs):
