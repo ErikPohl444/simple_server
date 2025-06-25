36024bd2fd5221a2f3c10683a4cd89818c8527f8
Erik Pohl <erik.pohl.444@gmail.com>
2025-06-25 08:53:11 -0400

    linting

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,10 @@
 import pytest
 from src import main
 
+
 def test_split_coordinates():
     assert main.split_coordinates("1_2_3") == [1, 2, 3]
+
 
 def test_directions_calculate_direction_move():
     directions = main.Directions(main.logger)
@@ -11,12 +13,14 @@ def test_directions_calculate_direction_move():
     assert isinstance(result, tuple)
     assert len(result) == 3
 
+
 def test_room_init():
     directions = main.Directions(main.logger)
     cipher = main.FernetCipher(b'0'*32, main.logger)
     room = main.Room("0_0_0", directions, cipher, main.logger)
     assert room.get_room_coordinates() == [0, 0, 0]
     assert isinstance(room.get_formatted_room_name(False), str)
+
 
 def test_maze_init():
     directions = main.Directions(main.logger)
